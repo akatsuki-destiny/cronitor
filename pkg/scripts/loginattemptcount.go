ea7cecbd5bd108381827ff1d87de9d7ce29acba1
kagan.meric <mustafakaganmeric@gmail.com>
2023-06-29 00:04:41 +0300

    update

--- pkg/scripts/loginattemptcount.go
+++ pkg/scripts/loginattemptcount.go
@@ -5,12 +5,16 @@ import (
 	"log"
 )
 
+// ResetLoginAttemptCounts is a function that resets login attempt counts
 func ResetLoginAttemptCounts() {
 
 	db := data.Init()
 	defer data.Close(db)
 
-	db.Exec("UPDATE users SET login_attempt_count = 0 WHERE login_attempt_count > 0")
+	result := db.Exec("UPDATE users SET login_attempt_count = 0 WHERE login_attempt_count > 0")
+	if result.Error != nil {
+		log.Default().Println("Error while resetting login attempt counts:", result.Error)
+	}
 
 	log.Default().Println("Login attempt counts reset.")
 
